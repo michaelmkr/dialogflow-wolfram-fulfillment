71cf8b908d111e67b77bc0bf88ca0caf45fdcaf8
MichaelMkr <it191501@fhstp.ac.at>
2019-10-16 13:15:43 +0200

    initial commit
    everything's working fine

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var axios = require("axios");
+app.use(express.json());
+
+
+var server = app.listen(3000, function () {
+    console.log("app running on port.", server.address().port);
+});
+
+var responseJSON = {"fulfillment_text": "Hello",};
+
+app.post('/', function (req, res) {
+    let JSONbody = req.body;
+    let userquery = req.body.queryResult.queryText;
+    let wolframAnswer = "";
+    let userqueryFormatted = userquery.replace(/ /g, "+");
+    axios
+        .get('http://api.wolframalpha.com/v1/result?appid=LE5L24-9A5G48VGPE&i=' + userqueryFormatted)
+        .then(function (response) {
+            wolframAnswer = response.data;
+            console.log("Innerhalb: " + wolframAnswer);
+            responseJSON.fulfillment_text = wolframAnswer;
+            res.json(responseJSON)
+        });
+});
+//TODO check response JSON format for Dialogflow
+
+
